9d258f7c22d739807f876413bcf00397b7e875b2
Amro dandashli <amrdandashli@gmail.com>
2021-05-13 07:18:14 +0000

    Done.

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ return 'I like to help people.'
     return 'You will see great things from Scuber.'
   }
   sayHi () {
-    return '"Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.'
+    return  `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
